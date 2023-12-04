07e34aa1501bacd6c4640c38ac674b980189815b
dbsrudrla <rladbsrud@korea.ac.kr>
2023-12-04 05:39:08 +0000

    feat: colors
    chore : colors
    
    fix : upload files
    
    feat : add files
    
    feat : color palette
    
    
    Co-authored-by: dbsrudrla <rladbsrud@korea.ac.kr>
    Co-authored-by: 윤경 김 <rladbsrud011120@naver.com>
    
    
    Merge-request: BUZZER-BEATER-MR-12
    Merged-by: 규성 임 <dla9319@korea.ac.kr>

---
+++ buzzerBeater-app/src/Common/Colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+    black : "#222222",
+    white : "#F8F6F6",
+    mainRed : "#E32424",
+    gray : "#979797",
+    warning : "#FFBBBB",
+    basketball : "#FE9738",
+}
+
+export default Colors;
