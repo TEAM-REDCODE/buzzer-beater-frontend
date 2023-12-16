123bf6b74416f6162bbe940fc4e484b6aee2e345
seonnnn <jjinjuu317@gmail.com>
2023-12-16 15:38:41 +0900

    feat:homescreen

--- buzzerBeater-app/App.js
+++ buzzerBeater-app/App.js
@@ -10,14 +10,15 @@ import SignUp from "./src/components/screen/SignUp";
 import MyPageScreen from './src/components/screen/MyPageScreen';
 import colors from './src/Common/Colors';
 import Start from './src/components/screen/Start';
+import Homes from './src/components/screen/Homes';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor : colors.black}}>
-      <Text>Home!!</Text>
-    </View>
-  );
-}
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor : colors.black}}>
+//       <Text>Home!!</Text>
+//     </View>
+//   );
+// }
 
 const Tab = createBottomTabNavigator();
 
@@ -26,12 +27,12 @@ export default function App() {
 
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Start'>
+        <Stack.Navigator initialRouteName='Homes'>
           <Stack.Screen name='Start' component={Start} options={{headerShown: false}}/>
           <Stack.Screen name='SignIn' component={SignIn} options={loginHeaderOptions}/>
           <Stack.Screen name='SignUp' component={SignUp} options={signUpHeaderOptions}/>
           <Stack.Screen name='MyPageScreen' component={MyPageScreen} options={myPageHeaderOptions}/>
-          <Stack.Screen name='HomeScreen' component={HomeScreen} options={commonHeaderOptions}/>
+          <Stack.Screen name='Homes' component={Homes} options={commonHeaderOptions}/>
           <Stack.Screen name='MercenaryList' component={MercenaryList} options={commonHeaderOptions}/>
         </Stack.Navigator>
     </NavigationContainer>
